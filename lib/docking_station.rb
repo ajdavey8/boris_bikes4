9cb575e796267cf4a234e1e63eaa6035667be288
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-26 11:52:48 +0100

    Added DockingStation capacity added test to check the capcity and raise and error if full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,22 @@
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bike_count
 
-  def initialize(bike)
-  @bike = bike
+  def initialize(bike_count)
+  @bike_count = bike_count
+
   end
 
   def release_bike
+    raise "There are no bikes at the docking station" if @bike_count == 0
     Bike.new
   end
+
+ def dock_bike
+   raise "DockingStation full." if @bike_count >= 10
+   @bike_count += 1
+
+ end
 
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
-   it { expect(DockingStation.new("bike")).to respond_to(:release_bike) }
-   it { expect(DockingStation.new("bike").release_bike.class).to eq Bike }
-   it { expect(DockingStation.new("bike").bike).to eq "bike" }
-   it { expect { DockingStation.new("nobikes").release_bike }.to raise_error("There are no bikes at the docking station")}
+   it { expect(DockingStation.new(1)).to respond_to(:release_bike) }
+   it { expect(DockingStation.new(1).release_bike.class).to eq Bike }
+   it { expect(DockingStation.new(1).bike_count).to eq 1 }
+   it { expect { DockingStation.new(0).release_bike }.to raise_error("There are no bikes at the docking station") }
+   it { expect { DockingStation.new(10).dock_bike }.to raise_error("DockingStation full.") }
+
 end
