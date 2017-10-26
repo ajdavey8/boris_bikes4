4fe7ce2f5ce3d72f6c05fe76913e6a57d24cedb7
Andrew Davey <ajdavey8@gmail.com>
2017-10-26 15:04:52 +0100

    added capacity to docking station, refactored release and dock bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,18 +3,20 @@ class DockingStation
   attr_reader :bike_count
 
   def initialize(bike_count)
-  @bike_count = bike_count
-
+    @bike_count = []
+    bike_count.times {@bike_count.push(Bike.new)}
   end
 
-  def release_bike
-    raise "There are no bikes at the docking station" if @bike_count == 0
-    Bike.new
+  def release_bike(amount_of_bikes)
+    raise "There are no bikes at the docking station" if @bike_count.empty?
+    last_bike = @bike_count.last(amount_of_bikes)
+    amount_of_bikes.times {@bike_count.pop}
+    last_bike
   end
 
- def dock_bike
-   raise "DockingStation full." if @bike_count >= 10
-   @bike_count += 1
+ def dock_bike(amount_of_bikes)
+   raise "DockingStation full." if @bike_count.count == 20
+   @bike_count += amount_of_bikes
 
  end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
    it { expect(DockingStation.new(1)).to respond_to(:release_bike) }
-   it { expect(DockingStation.new(1).release_bike.class).to eq Bike }
-   it { expect(DockingStation.new(1).bike_count).to eq 1 }
-   it { expect { DockingStation.new(0).release_bike }.to raise_error("There are no bikes at the docking station") }
-   it { expect { DockingStation.new(10).dock_bike }.to raise_error("DockingStation full.") }
-
-end
+   it { expect(DockingStation.new(1).release_bike(1)[0].class).to eq Bike }
+   it { expect(DockingStation.new(1).bike_count[0].class).to eq Bike }
+   it { expect { DockingStation.new(0).release_bike(1) }.to raise_error("There are no bikes at the docking station") }
+   it { expect { DockingStation.new(20).dock_bike(1) }.to raise_error("DockingStation full.") }
+ end
