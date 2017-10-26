c059eff670803d1aed69c1a9c31bcd16527b20ee
Marco Vanali <Marco91Vanali@gmail.com>
2017-10-26 16:08:23 +0100

    Created empty? and full? methods, refracted release_bike, dock_bike and initialize method.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,19 +4,32 @@ class DockingStation
 
   def initialize(bike_count)
     @bike_count = []
-    bike_count.times {@bike_count.push(Bike.new)}
+    bike_count.times { @bike_count << Bike.new }
   end
 
+   private
+
+  def empty?
+    @bike_count.count == 0 ? true : false
+  end
+
+   def full?
+     @bike_count.count >= 20 ? true : false
+   end
+
+  public
+
   def release_bike(amount_of_bikes)
-    raise "There are no bikes at the docking station" if @bike_count.empty?
+    raise "There are no bikes at the docking station" if empty?
     last_bike = @bike_count.last(amount_of_bikes)
     amount_of_bikes.times {@bike_count.pop}
     last_bike
   end
 
  def dock_bike(amount_of_bikes)
-   raise "DockingStation full." if @bike_count.count == 20
-   @bike_count += amount_of_bikes
+
+   raise "DockingStation full." if full?
+   amount_of_bikes.times { @bike_count << Bike.new }
 
  end
 
